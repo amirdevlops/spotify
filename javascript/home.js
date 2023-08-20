991d821c6aaf0375c4265bdd51fd07c91204a9eb
amirdevlops <137190254+amirdevlops@users.noreply.github.com>
2023-08-20 14:29:57 +0530

    Add files via upload

--- javascript/home.js
+++ javascript/home.js
@@ -1,27 +1,64 @@
-let  ulSlider = document.getElementById("ulSlider");
-let isdragging = false, startX, startScrollLeft;
-const dragStart = (e)=>{
-    isdragging = true;
-    ulSlider.classList.add("dragging");
-
-    startX = e.pageX;
-    startScrollLeft = ulSlider.scrollLeft;
-
-} 
-
-const dragging = (e) =>{
-if(isdragging == false){
-    return;
-}    
-ulSlider.scrollLeft= startScrollLeft - (e.pageX - startX);
-
+// Function to check if the user is on a mobile device
+function isMobileDevice() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 }
 
-const draggStop = ()=>{
-    isdragging = false;
-    ulSlider.classList.remove("dragging")
-}
+// Choose the appropriate version of the script based on the device type
+if (isMobileDevice()) {
+    // Mobile version of the script
+    let ulSlider = document.getElementById("ulSlider");
+    let isDragging = false, startX, startScrollLeft;
 
-ulSlider.addEventListener("mousedown", dragStart);
-ulSlider.addEventListener("mousemove", dragging);
-ulSlider.addEventListener("mouseup", draggStop);
+    const dragStart = (e) => {
+        isDragging = true;
+        ulSlider.classList.add("dragging");
+
+        startX = e.touches[0].pageX;
+        startScrollLeft = ulSlider.scrollLeft;
+    }
+
+    const dragging = (e) => {
+        if (!isDragging) {
+            return;
+        }
+        ulSlider.scrollLeft = startScrollLeft - (e.touches[0].pageX - startX);
+    }
+
+    const dragStop = () => {
+        isDragging = false;
+        ulSlider.classList.remove("dragging");
+    }
+
+    ulSlider.addEventListener("touchstart", dragStart);
+    ulSlider.addEventListener("touchmove", dragging);
+    ulSlider.addEventListener("touchend", dragStop);
+} else {
+    // Desktop version of the script
+    let ulSlider = document.getElementById("ulSlider");
+    let isDragging = false, startX, startScrollLeft;
+
+    const dragStart = (e) => {
+        isDragging = true;
+        ulSlider.classList.add("dragging");
+
+        startX = e.pageX;
+        startScrollLeft = ulSlider.scrollLeft;
+
+    }
+
+    const dragging = (e) => {
+        if (!isDragging) {
+            return;
+        }
+        ulSlider.scrollLeft = startScrollLeft - (e.pageX - startX);
+    }
+
+    const dragStop = () => {
+        isDragging = false;
+        ulSlider.classList.remove("dragging");
+    }
+
+    ulSlider.addEventListener("mousedown", dragStart);
+    ulSlider.addEventListener("mousemove", dragging);
+    ulSlider.addEventListener("mouseup", dragStop);
+}
